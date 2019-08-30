7b67bf7ddccb2423377475b1efa09b6b1bbfb20e
Nazmi Altun <nazmialtun@windowslive.com>
2019-08-30 23:01:13 +0300

    code cleanup

--- src/App/pages/home/sagas.ts
+++ src/App/pages/home/sagas.ts
@@ -38,16 +38,18 @@ function* handleFetch() {
     identitApiRes.data.forEach((user: any) => {
       webApiRes.data.forEach((apiUser: User) => {
         if (user.id === apiUser.subjectId) {
-          var combinedUser : User = {
+          var combinedUser: User = {
             ...user,
             ...apiUser,
             licenseTypeName: _.find(licenseTypes, ['id', user.licenseTypeId]).name,
             status: getStatus(user.active, user.expiryDateUtc),
           };
           combinedUser.expiryDateUtc = _.isUndefined(user.expiryDateUtc) ? undefined : new Date(user.expiryDateUtc);
-          combinedUser.startDateTimeUtc = _.isUndefined(user.startDateTimeUtc) ? undefined : new Date(user.startDateTimeUtc);
+          combinedUser.startDateTimeUtc = _.isUndefined(user.startDateTimeUtc)
+            ? undefined
+            : new Date(user.startDateTimeUtc);
           combinedUser.createdDateTimeUtc = new Date(user.createdDateTimeUtc);
-          combinedUser.status = getStatus(combinedUser.active,combinedUser.expiryDateUtc);
+          combinedUser.status = getStatus(combinedUser.active, combinedUser.expiryDateUtc);
           users.push(combinedUser);
         }
       });
