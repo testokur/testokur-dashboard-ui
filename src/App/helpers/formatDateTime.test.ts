5afa74e44b0dff2ab8d42b741a08f5da0c49a1a2
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-03 18:05:33 +0800

    code formatting

--- src/App/helpers/formatDateTime.test.ts
+++ src/App/helpers/formatDateTime.test.ts
@@ -1,11 +1,11 @@
-import  formatDateTime from './formatDateTime';
+import formatDateTime from './formatDateTime';
 
 test('when datetime is undefined then given string should return', () => {
-  const output  = formatDateTime(undefined,'-');
+  const output = formatDateTime(undefined, '-');
   expect(output).toBe('-');
 });
 
 test('when datetime is valid then it should be formatted in default format', () => {
-  const output  = formatDateTime(new Date(2018, 11, 24, 10, 33, 30, 0),'-');
+  const output = formatDateTime(new Date(2018, 11, 24, 10, 33, 30, 0), '-');
   expect(output).toBe('24.12.2018 10:33');
 });
