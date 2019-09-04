c1947cd8c40b7b90f7432d9ff61b4292ae4a869e
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-04 13:14:19 +0800

    code formatting

--- src/App/helpers/api.ts
+++ src/App/helpers/api.ts
@@ -8,13 +8,13 @@ export const HeaderNames = {
 axios.defaults.headers.post[HeaderNames.ContentType] = 'application/json';
 axios.defaults.headers.common[HeaderNames.Authorization] = `Bearer ${localStorage.getItem('access_token')}`;
 
-export const createIdentityApiClient = (timeOut:number = 60000) =>
+export const createIdentityApiClient = (timeOut: number = 60000) =>
   axios.create({
     baseURL: window._env_.identityApiUrl,
     timeout: timeOut,
   });
 
-export const createWebApiClient = (timeOut:number = 60000) =>
+export const createWebApiClient = (timeOut: number = 60000) =>
   axios.create({
     baseURL: window._env_.webapiUrl,
     timeout: timeOut,
