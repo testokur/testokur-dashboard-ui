c57cced1d760937caa0fd3f75ff6f91229f7a23e
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-15 06:46:19 +0300

    minor clean-up in deploy prod script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 docker load -i /home/images/dashboard.tar
-docker stop testokur-dashboard
-docker rm --force testokur-dashboard
+docker stop testokur-dashboard && docker rm --force testokur-dashboard
 docker run -d  \
-  --env-file  /home/env/dashboard.env \
+    --env-file  /home/env/dashboard.env \
 	--name testokur-dashboard \
 	--restart=always  \
 	--network=testokur \
