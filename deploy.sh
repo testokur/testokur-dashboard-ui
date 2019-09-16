b3e078a6676eaa84843bdaea6f5e8af19db38ef5
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-16 14:01:54 +0300

    Using docker hub instead of file transfer

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-docker load -i /home/images/dashboard.tar
-docker stop testokur-dashboard-qa
-docker rm --force testokur-dashboard-qa
+docker pull nazmialtun/testokur-dashboard-ui:latest
+docker stop testokur-dashboard-qa && docker rm --force testokur-dashboard-qa
 docker run -d  \
   --env-file  /home/env/dashboard-qa.env \
 	--name testokur-dashboard-qa \
 	--restart=always  \
 	--network=testokur \
 	--network-alias=dashboard-qa \
-	testokur-dashboard:latest
+	nazmialtun/testokur-dashboard-ui:latest
