f28beb4c0fe73e87c71e6093520cc3bacc353256
Nazmi Altun <nazmialtun@windowslive.com>
2019-09-19 16:41:52 +0300

    margin-top for online users

--- src/App/pages/home/component.tsx
+++ src/App/pages/home/component.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { default as NewUsers } from './NewUsers';
 import { OnlineUsers } from './OnlineUsers';
+import { Box } from '@material-ui/core';
 
 export const component = () => {
   return (
     <div>
       <NewUsers />
-      <OnlineUsers />
+      <Box mt={10}><OnlineUsers /></Box>
     </div>
   );
 };
