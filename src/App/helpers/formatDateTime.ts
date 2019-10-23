6e292dfcb859b73d08ef9c864185ffde70d918e3
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-23 23:30:48 +0800

    secondsa added to the format

--- src/App/helpers/formatDateTime.ts
+++ src/App/helpers/formatDateTime.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash';
 import fecha from 'fecha';
 
-const defaultFormat = 'DD.MM.YYYY HH:mm';
+const defaultFormat = 'DD.MM.YYYY HH:mm:ss';
 
 export default (date: Date | undefined, undefinedString: string = '-') => {
   return _.isUndefined(date) ? undefinedString : fecha.format(date, defaultFormat);
