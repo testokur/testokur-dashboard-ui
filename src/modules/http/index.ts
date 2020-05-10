f081aa8432917c01c4003a1b80f0a969b881faaf
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-11 00:39:44 +0800

    Use httpclient from testokur-utils

--- src/modules/http/index.ts
+++ src/modules/http/index.ts
@@ -1,46 +1,7 @@
-class Client {
-  private baseUrl: string;
+import { HttpClient } from 'testokur-utils';
 
-  public constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
-  }
-
-  public async get(url: string): Promise<any> {
-    const response = await fetch(this.createUrl(url), this.createRequest('GET'));
-
-    return await response.json();
-  }
-
-  public async post(url: string, data?: any): Promise<Response> {
-    return await fetch(this.createUrl(url), this.createRequest('POST', data));
-  }
-
-  public async delete(url: string): Promise<string> {
-    const response = await fetch(this.createUrl(url), this.createRequest('DELETE'));
-
-    return await response.text();
-  }
-
-  private createUrl(url: string): string {
-    const fullUrl = `${this.baseUrl}/${url}`;
-    return fullUrl.replace(/([^:]\/)\/+/g, '$1');
-  }
-
-  private createRequest(method: string, data?: any): RequestInit {
-    const request: RequestInit = {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
-      },
-      body: data === undefined ? undefined : JSON.stringify(data),
-    };
-    return request;
-  }
-}
-
-export const webApiClient = new Client(window._env_.webapiUrl);
-export const identityApiClient = new Client(window._env_.identityApiUrl);
-export const notificationApiClient = new Client(window._env_.notificationApiUrl);
-export const reportApiClient = new Client(window._env_.reportApiUrl);
-export const sabitApiClient = new Client(window._env_.sabitApiUrl);
+export const webApiClient = new HttpClient(window._env_.webapiUrl);
+export const identityApiClient = new HttpClient(window._env_.identityApiUrl);
+export const notificationApiClient = new HttpClient(window._env_.notificationApiUrl);
+export const reportApiClient = new HttpClient(window._env_.reportApiUrl);
+export const sabitApiClient = new HttpClient(window._env_.sabitApiUrl);
