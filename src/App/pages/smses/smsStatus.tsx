c7e1d9eddac7a851bfb51ace97a84f13bae90a59
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-26 02:20:40 +0800

    use testokur-ui components in sms status

--- src/App/pages/smses/smsStatus.tsx
+++ src/App/pages/smses/smsStatus.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { get } from 'testokur-utils';
-import { Chip, makeStyles, Theme } from '@material-ui/core';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import HourglassFull from '@material-ui/icons/HourglassFull';
-import AccessTime from '@material-ui/icons/AccessTime';
-import Cancel from '@material-ui/icons/Cancel';
-import { green, amber } from '@material-ui/core/colors';
-import clsx from 'clsx';
+import { Badge, BadgeTypes, Expired, Time, Check, Cancelled } from 'testokur-ui';
 
 export const SmsStatuses = {
   Failed: 'Hata Almis',
@@ -20,59 +13,35 @@ interface Props {
 }
 
 class Status {
-  public readonly name: string;
+  public readonly badgeType: BadgeTypes;
   public readonly icon: any;
   public readonly label: string;
 
-  public constructor(name: string, icon: any, label: string) {
-    this.name = name;
+  public constructor(badgeType: BadgeTypes, icon: any, label: string) {
+    this.badgeType = badgeType;
     this.icon = icon;
     this.label = label;
   }
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  chip: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(2),
-    fontSize: 12,
-  },
-  successful: {
-    backgroundColor: green[600],
-  },
-  tryagain: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  failed: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  pending: {
-    backgroundColor: amber[700],
-  },
-}));
-
 function getStatus(props: Props): Status {
   if (props.status === 0) {
-    return new Status('pending', <AccessTime />, SmsStatuses.Pending);
+    return new Status(BadgeTypes.Warning, <Time />, SmsStatuses.Pending);
   }
   if (props.status === 1) {
-    return new Status('successful', <CheckCircleOutlineIcon />, SmsStatuses.Successful);
+    return new Status(BadgeTypes.Success, <Check />, SmsStatuses.Successful);
   }
   if (props.status === 2) {
-    return new Status('tryagain', <HourglassFull />, SmsStatuses.TryingAgain);
+    return new Status(BadgeTypes.CriticalInverted, <Expired />, SmsStatuses.TryingAgain);
   }
-  return new Status('failed', <Cancel />, SmsStatuses.Failed);
+  return new Status(BadgeTypes.Critical, <Cancelled />, SmsStatuses.Failed);
 }
 
 export const SmsStatus = (props: Props) => {
-  const classes = useStyles();
   const status = getStatus(props);
   return (
-    <Chip
-      color="secondary"
-      className={clsx(classes.chip, get(classes, `${status.name}`))}
-      label={status.label}
-      icon={status.icon}
-    />
+    <Badge type={status.badgeType} ariaLabel={status.label} icon={status.icon}>
+      {status.label}
+    </Badge>
   );
 };
